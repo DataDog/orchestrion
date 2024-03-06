a6cf3f2873178e9b375671a6b4a06e465225ec14
François Mazeau <francois.mazeau@datadoghq.com>
2024-03-06 16:23:50 +0100

    toolexec: enhance tests.sh

--- internal/toolexec/tests/tests.sh
+++ internal/toolexec/tests/tests.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-set -x
-set -e
+set -exuo pipefail
+
 testDir=$(realpath $(dirname $0))
+cacheDir=$(mktemp -d $TMPDIR/gocache-XXXXX)
 cd $testDir
 go build -a -o proxy ./proxy
 
 for pkg in pkg_*
 do
     # Inject $pkg into the base package
-    go build -a -o main -toolexec "$testDir/proxy/proxy $testDir/$pkg/cfg.yaml" ./base
+    GOCACHE=$cacheDir go build -a -o main -toolexec "$testDir/proxy/proxy $testDir/$pkg/cfg.yaml" ./base
     out=$(./main)
     # Make sure running the program yields the instrumented output
-    [ "$out" = "$pkg" ]
+    diff  <(echo "$out") <(echo "$pkg")
     rm -f main
 done
 
 rm -f proxy/proxy
+rm -r $cacheDir
