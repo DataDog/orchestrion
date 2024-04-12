7a1e3d3b64960afc2f19e54bd354194429da91a5
François Mazeau <francois.mazeau@datadoghq.com>
2024-04-12 10:59:31 +0200

    Inject Go command flags in go list too

--- internal/toolexec/aspect/resolve.go
+++ internal/toolexec/aspect/resolve.go
@@ -17,6 +17,16 @@ import (
 	"github.com/datadog/orchestrion/internal/goflags"
 )
 
+func prepareGoCommandArgs(cmd string, args ...string) ([]string, error) {
+	flags, err := goflags.Flags()
+	if err != nil {
+		return nil, fmt.Errorf("retrieving go command flags: %v", err)
+	}
+	compound := append(flags.Slice(), args...)
+	compound = append([]string{cmd}, compound...)
+	return compound, nil
+}
+
 // resolvePackageFiles attempts to retrieve the archive for the designated import path. It attempts
 // to locate the archive for `importPath` and its dependencies using `go list` first, which may
 // return entries from the GOCACHE. If that fails or a component is stale, `go build` will be
@@ -25,13 +35,18 @@ import (
 func resolvePackageFiles(importPath string) (map[string]string, error) {
 	// Apply quoting as appropriate to avoid shell interpretation issues...
 	toolexec := fmt.Sprintf("%q %q", os.Args[0], os.Args[1])
+	// Retrieve parent Go command flags
 
 	attemptedBuild := false // Whether we attempted a build already or not
 	for {
-		cmd := exec.Command("go", "list", "-toolexec", toolexec, "-json", "-deps", "-export", "--", importPath)
+		args, err := prepareGoCommandArgs("list", "-toolexec", toolexec, "-json", "-deps", "-export", "--", importPath)
+		if err != nil {
+			return nil, fmt.Errorf("preparing go command %v: %w", args, err)
+		}
+		cmd := exec.Command("go", args...)
 		var stdout bytes.Buffer
 		cmd.Stdout = &stdout
-		if err := cmd.Run(); err != nil {
+		if err = cmd.Run(); err != nil {
 			return nil, fmt.Errorf("running %q: %w", cmd.Args, err)
 		}
 
@@ -54,7 +69,6 @@ func resolvePackageFiles(importPath string) (map[string]string, error) {
 		}
 
 		output := make(map[string]string, len(items))
-		var err error
 		for _, item := range items {
 			if item.Standard && item.ImportPath == "unsafe" && item.Export == "" {
 				// Special-casing "unsafe", because it's not provided like other modules
@@ -75,13 +89,10 @@ func resolvePackageFiles(importPath string) (map[string]string, error) {
 
 		// Not found or stale -- let's try to `go build` it so it's present and not stale.
 		// Retrieve original go command flags and pass them along
-		flags, err := goflags.Flags()
+		args, err = prepareGoCommandArgs("build", "-toolexec", toolexec, "--", importPath)
 		if err != nil {
 			return nil, fmt.Errorf("retrieving go command flags: %v", err)
 		}
-		flagsSlice := flags.Slice()
-		args := append([]string{"build"}, flagsSlice...)
-		args = append(args, "-toolexec", toolexec, "--", importPath)
 		if err := exec.Command("go", args...).Run(); err != nil {
 			return nil, fmt.Errorf("building %q: %w", importPath, err)
 		}
