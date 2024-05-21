64266f3f27adceedab6ba318c7f64e5d496c42f4
Romain Marcadier <romain@datadoghq.com>
2024-05-21 13:26:47 +0200

    Build into temporary directory

--- integration-tests.ps1
+++ integration-tests.ps1
@@ -1,1 +1,15 @@
 #!/usr/bin/env pwsh
+
+function New-TemporaryDirectory {
+  $parent = [System.IO.Path]::GetTempPath()
+  $name = [System.IO.Path]::GetRandomFileName()
+  New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+$TmpDir = New-TemporaryDirectory
+try {
+  go -C "_integration-tests" build -o $TmpDir ./tests/...
+} finally {
+  # Clean up the temporary directory
+  Remove-Item -Recurse -Force $TmpDir
+}
