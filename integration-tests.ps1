9a967c2c62d592ed8c74b1b7fba362c8374ac669
Romain Marcadier <romain@datadoghq.com>
2024-05-21 13:36:31 +0200

    Matrix on go version

--- integration-tests.ps1
+++ integration-tests.ps1
@@ -8,7 +8,16 @@ function New-TemporaryDirectory {
 
 $TmpDir = New-TemporaryDirectory
 try {
-  go -C "_integration-tests" build -o $TmpDir ./tests/...
+  # Build orchestrion
+  $orchestrion = Join-Path $TmpDir "orchestrion.exe"
+  go build -o $orchestrion .
+
+  # Warm up orchestrion
+  $orchestrion warmup
+
+  # Build the test cases
+  $testBin = (Join-Path $TmpDir "tests")
+  $orchestrion go -C "_integration-tests" build -o $testBin ./tests/...
 } finally {
   # Clean up the temporary directory
   Remove-Item -Recurse -Force $TmpDir
