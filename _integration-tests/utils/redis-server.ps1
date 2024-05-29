f27c055be546261c3d50793167c559a8c3575954
Romain Marcadier <romain@datadoghq.com>
2024-05-29 12:44:17 +0200

    PowerShell line continuation is a backtick, not a backslash

--- _integration-tests/utils/redis-server.ps1
+++ _integration-tests/utils/redis-server.ps1
@@ -13,11 +13,11 @@ catch
 
   if ($IsLinux)
   {
-    sudo apt update && \
-      sudo apt install -y lsb-release curl gpg && \
-      curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg && \
-      Write-Output "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list && \
-      sudo apt update && \
+    sudo apt update && `
+      sudo apt install -y lsb-release curl gpg && `
+      curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg && `
+      Write-Output "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list && `
+      sudo apt update && `
       sudo apt install redis
   }
   elseif ($IsMacOS)
