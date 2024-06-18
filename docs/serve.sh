53379204b696652ae2332fcb9ef32e46e2a81d90
Romain Marcadier <romain@datadoghq.com>
2024-06-18 16:30:03 +0200

    Remove generated docs content from git tracking

--- docs/serve.sh
+++ docs/serve.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+go -C .. generate ./internal/injector/builtin
 git submodule update --init --recursive
 go run github.com/gohugoio/hugo serve "$@"
