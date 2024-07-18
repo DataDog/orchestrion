ae441b05c0ea9831ec1efef52f2555e5bbc7ecdd
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2024-07-18 15:34:51 +0200

    trying to debug the injector
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- internal/injector/builtin/generated.go
+++ internal/injector/builtin/generated.go
@@ -583,7 +583,7 @@ var Aspects = [...]aspect.Aspect{
 		),
 		Advice: []advice.Advice{
 			advice.PrependStmts(code.MustTemplate(
-				"parent, _ := dyngo.FromContext(nil)\nif parent != nil {\n\top := &ossec.OpenOperation{\n        Operation: dyngo.NewOperation(parent),\n    }\n\n    var __dd_block bool\n    dyngo.OnData(op, func(_ *events.BlockingSecurityEvent) {\n        __dd_block = true\n    })\n\n    dyngo.StartOperation(op, ossec.OpenOperationArgs{\n        Path: {{ .Function.Argument 0 }},\n        Flags: {{ .Function.Argument 1 }},\n        Perms: {{ .Function.Argument 2 }},\n    })\n\n    var __dd_file any = {{ .Function.Returns 0 }}\n    defer dyngo.FinishOperation(op, ossec.OpenOperationRes{\n        File: &__dd_file,\n        Err: &{{ .Function.Returns 1 }},\n    })\n\n    if __dd_block {\n        return\n    }\n}",
+				"println(\"os.OpenFile\")\nparent, _ := dyngo.FromContext(nil)\nif parent != nil {\n    println(\"os.OpenFile: parent is not nil\")\n\top := &ossec.OpenOperation{\n        Operation: dyngo.NewOperation(parent),\n    }\n\n    var __dd_block bool\n    dyngo.OnData(op, func(_ *events.BlockingSecurityEvent) {\n        __dd_block = true\n    })\n\n    dyngo.StartOperation(op, ossec.OpenOperationArgs{\n        Path: {{ .Function.Argument 0 }},\n        Flags: {{ .Function.Argument 1 }},\n        Perms: {{ .Function.Argument 2 }},\n    })\n\n    var __dd_file any = {{ .Function.Returns 0 }}\n    defer dyngo.FinishOperation(op, ossec.OpenOperationRes{\n        File: &__dd_file,\n        Err: &{{ .Function.Returns 1 }},\n    })\n\n    if __dd_block {\n        return\n    }\n}",
 				map[string]string{
 					"dyngo":  "gopkg.in/DataDog/dd-trace-go.v1/internal/appsec/dyngo",
 					"events": "gopkg.in/DataDog/dd-trace-go.v1/appsec/events",
@@ -691,4 +691,4 @@ var InjectedPaths = [...]string{
 }
 
 // Checksum is a checksum of the built-in configuration which can be used to invalidate caches.
-const Checksum = "sha512:rmpDuWJdisTgkELPQtRu9B0xSKkWcLVhk3FjZ4zOAW5Vjyd5lrcK1H9f/VHp3jWHPTRI3Y8vGx87HUm1Ato7hg=="
+const Checksum = "sha512:VsMepJyMOWnk98taMcYIoQsbvYha7AZbtvrNmFUpPNoVMSpdm06C/XyYzk6bFTmP0yFd/RQ7oqxR2apv0FkF3A=="
