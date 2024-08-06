745a9ef39b70f40fe37acc021eb167047ec36b95
Nick Ripley <nick.ripley@datadoghq.com>
2024-08-06 08:40:07 -0400

    feat: add log/slog instrumentation (#208)
    ### What does this PR do?
    
    Add instrumentation for `log/slog`. Implemented using dd-trace-go's
    `log/slog.Handler` wrapper to wrap any handler passed to `log/slog.New`.
    
    This commit does not instrument the default `log/slog.Logger`. We could
    do that, for example, by adding some logic like this to `func main()` if
    `log/slog` is imported by package main:
    
    ```go
    slog.SetDefault(
        slog.New(
            slogtrace.WrapHandler(
                slog.Default().Handler(),
            ),
        ),
    )
    ```
    
    Will leave that as a followup for now.
    
    ### Motivation
    
    I maintain an app that uses `log/slog` for logging. I'd like to
    eventually
    instrument the app using Orchestrion, and having trace-log correlation
    is very
    useful for debugging.
    
    ### Reviewer's Checklist
    <!--
    * Authors can use this list as a reference to ensure that there are no
    problems
    during the review but the signing off is to be done by the reviewer(s).
    -->
    
    - [x] Changed code has unit tests for its functionality.
    
    ---------
    
    Co-authored-by: Romain Marcadier <romain@datadoghq.com>
    Co-authored-by: Romain Marcadier <romain.muller@telecomnancy.net>

--- internal/injector/builtin/generated.go
+++ internal/injector/builtin/generated.go
@@ -569,6 +569,18 @@ var Aspects = [...]aspect.Aspect{
 			), []string{}),
 		},
 	},
+	// From stdlib/slog.yml
+	{
+		JoinPoint: join.FunctionCall("log/slog.New"),
+		Advice: []advice.Advice{
+			advice.WrapExpression(code.MustTemplate(
+				"{{ .AST.Fun }}(slogtrace.WrapHandler({{ index .AST.Args 0 }}))",
+				map[string]string{
+					"slogtrace": "gopkg.in/DataDog/dd-trace-go.v1/contrib/log/slog",
+				},
+			)),
+		},
+	},
 }
 
 // RestorerMap is a set of import path to name mappings for packages that would be incorrectly named by restorer.Guess
@@ -621,6 +633,7 @@ var InjectedPaths = [...]string{
 	"gopkg.in/DataDog/dd-trace-go.v1/contrib/jinzhu/gorm",
 	"gopkg.in/DataDog/dd-trace-go.v1/contrib/k8s.io/client-go/kubernetes",
 	"gopkg.in/DataDog/dd-trace-go.v1/contrib/labstack/echo.v4",
+	"gopkg.in/DataDog/dd-trace-go.v1/contrib/log/slog",
 	"gopkg.in/DataDog/dd-trace-go.v1/contrib/net/http",
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace",
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/ext",
@@ -638,4 +651,4 @@ var InjectedPaths = [...]string{
 }
 
 // Checksum is a checksum of the built-in configuration which can be used to invalidate caches.
-const Checksum = "sha512:xVdleTJLF/JLrj8doJ5FOOIt0Z1yoB6ObmUStg98Kw1vB6h6ieCvS9vWwVG77HxXz+NRrVGK0mUoGizxsJVcjQ=="
+const Checksum = "sha512:unRxDnXhsMQzDuBovnYYIsd3o308PwmY2RtYDj+LXxKyi7DRydsAjazHrt/WkmuItBOt/gV4QDQxlTQS/AWdBQ=="
