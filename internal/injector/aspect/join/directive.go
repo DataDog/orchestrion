c443e4f1e7f0a65507ea7fcd17cf27c28410f2af
Nick Ripley <nick.ripley@datadoghq.com>
2024-08-07 09:07:15 -0400

    perf: switch to regexp-less directive matching (#212)
    ### What does this PR do?
    
    The Directive join point uses a regex to check whether the directive for
    a node matches the desired directive. Currently this regex is compiled
    every time the Directive is evaluated against a node. This shows up as a
    pretty big hot spot in CPU profiles of Orchestrion. We only need to
    compile the regex one time, when the Directive is created.
    
    Better yet - the regexp matching that is done here is simple enough that
    we can switch to much leaner string processing, and do away with regular
    expression in this code path entirely!
    
    ### Motivation
    
    I noticed that the integration tests are really slow, like ~2 hours in a
    recent
    run. Most of that time seems to be the Orchestrion build of the
    integration
    test suite, given how short the actual test runs are in this example
    compared
    to the overall run time:
    
    https://github.com/DataDog/orchestrion/actions/runs/10269117098/job/28423767869?pr=178#step:7:214
    
    I tried out profiling Orchestrion to look for obvious hot spots. I did
    this by
    basically adding a way to make Orchestrion generate a profile each time
    it
    runs, like `orchestrion-${pid}.pprof`, and then viewing the combined
    profiles.
    I can send a PR if we want the option, although it produces TONS of
    files since
    Orchestrion is invoked a lot.
    
    At any rate, this PR addresses one of the bigger hot spots:
    
    <img width="1509" alt="Screenshot 2024-08-06 at 17 09 21"
    src="https://github.com/user-attachments/assets/eec385d9-26eb-4e02-8b93-44ce7f9d67c5">
    
    We could do this without a regex if we want to squeeze even more
    overhead out of it. I _think_ something like this would do the job?
    
    ```
    func matches(s, target string) bool {
    	s = strings.TrimLeftFunc(s, unicode.IsSpace)
    	if !strings.HasPrefix(s, target) {
    		return false
    	}
    	rest := s[len(target):]
    	if len(rest) == 0 {
    		return true
    	}
    	r, _ := utf8.DecodeRuneInString(rest)
    	return unicode.IsSpace(r)
    }
    ```
    
    Benchmarks:
    
    ```
    BenchmarkRegexCompiledEachTime-10    	  463681	      2442 ns/op	    4790 B/op	      37 allocs/op
    BenchmarkRegexPreCompiled-10         	 4998306	       242.1 ns/op	       0 B/op	       0 allocs/op
    BenchmarkNotRegex-10                 	63743888	        17.97 ns/op	       0 B/op	       0 allocs/op
    ```
    
    ### Reviewer's Checklist
    <!--
    * Authors can use this list as a reference to ensure that there are no
    problems
    during the review but the signing off is to be done by the reviewer(s).
    -->
    
    - [x] Changed code has unit tests for its functionality.
    
    ---------
    
    Co-authored-by: Romain Marcadier <romain@datadoghq.com>

--- internal/injector/aspect/join/directive.go
+++ internal/injector/aspect/join/directive.go
@@ -7,7 +7,9 @@ package join
 
 import (
 	"fmt"
-	"regexp"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 
 	"github.com/datadog/orchestrion/internal/injector/node"
 	"github.com/dave/dst"
@@ -22,9 +24,8 @@ func Directive(name string) directive {
 }
 
 func (d directive) Matches(node *node.Chain) bool {
-	re := regexp.MustCompile(fmt.Sprintf(`\s*//%s(?:\s.*)?$`, regexp.QuoteMeta(string(d))))
 	for _, dec := range node.Node.Decorations().Start.All() {
-		if re.MatchString(dec) {
+		if d.matches(dec) {
 			return true
 		}
 	}
@@ -44,6 +45,27 @@ func (d directive) Matches(node *node.Chain) bool {
 	return false
 }
 
+func (d directive) matches(dec string) bool {
+	// Trim leading white space
+	rest := strings.TrimLeftFunc(dec, unicode.IsSpace)
+
+	// Check we have a single-line comment
+	if !strings.HasPrefix(rest, "//") {
+		return false
+	}
+
+	// Check the // is followed immediately by the directive name
+	rest = rest[2:]
+	if !strings.HasPrefix(rest, string(d)) {
+		return false
+	}
+
+	// If there is something after the directive name, it must be white space
+	rest = rest[len(d):]
+	rune, size := utf8.DecodeRuneInString(rest)
+	return size == 0 || unicode.IsSpace(rune)
+}
+
 func (directive) ImpliesImported() []string {
 	return nil
 }
@@ -53,7 +75,7 @@ func (d directive) AsCode() jen.Code {
 }
 
 func (d directive) RenderHTML() string {
-	return fmt.Sprintf(`<div class="flex join-point directive"><span class="type">Has directive</span><code>//%s</code></div>`, d)
+	return fmt.Sprintf(`<div class="flex join-point directive"><span class="type">Has directive</span><code>//%s</code></div>`, string(d))
 }
 
 func init() {
