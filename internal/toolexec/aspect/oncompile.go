6f1942f0a6236c336668cb2c62a7eb7513af64c5
Romain Marcadier <romain@datadoghq.com>
2024-08-19 19:55:10 +0200

    feat: improved line information preservation
    Use a new and improved method to preserve line information from the
    original source file into the modified AST; by comparing information
    about original nodes in the decorator's view with those in the
    restorer's.
    
    This ensures better correspondance, emits fewer line directives, and
    shoudl result in better debugging experience on instrumented code.

--- internal/toolexec/aspect/oncompile.go
+++ internal/toolexec/aspect/oncompile.go
@@ -13,7 +13,6 @@ import (
 	"regexp"
 	"slices"
 	"strings"
-	"unsafe"
 
 	"github.com/datadog/orchestrion/internal/injector"
 	"github.com/datadog/orchestrion/internal/injector/aspect"
@@ -98,19 +97,6 @@ func (w Weaver) OnCompile(cmd *proxy.CompileCommand) error {
 			if err := cmd.ReplaceParam(gofile, res.Filename); err != nil {
 				return fmt.Errorf("replacing %q with %q: %w", gofile, res.Filename, err)
 			}
-		}
-
-		outDir := filepath.Join(orchestrionDir, "src", "synthetic")
-		for name, data := range res.NewFiles {
-			log.Debugf("New source file: %q\n%s\n", name, unsafe.String(unsafe.SliceData(data), len(data)))
-			if err := os.MkdirAll(outDir, 0o755); err != nil {
-				return fmt.Errorf("creating synthetic source directory %q: %w", outDir, err)
-			}
-			filename := filepath.Join(outDir, name)
-			if err := os.WriteFile(filename, data, 0o644); err != nil {
-				return fmt.Errorf("writing synthetic source file %q: %w", filename, err)
-			}
-			cmd.AddFiles([]string{filename})
 		}
 
 		references.Merge(res.References)
