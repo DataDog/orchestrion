96f73f5b83f08a24cec276a6545b878aa90c229a
Eliott Bouhana <eliott.bouhana@datadoghq.com>
2024-08-29 09:57:04 +0200

    chore: rename rasp.json
    Signed-off-by: Eliott Bouhana <eliott.bouhana@datadoghq.com>

--- _integration-tests/tests/os/lfi.go
+++ _integration-tests/tests/os/lfi.go
@@ -29,7 +29,7 @@ type TestCase struct {
 }
 
 func (tc *TestCase) Setup(t *testing.T) {
-	t.Setenv("DD_APPSEC_RULES", "./testdata/rasp.json")
+	t.Setenv("DD_APPSEC_RULES", "./testdata/rasp-only-rules.json")
 	t.Setenv("DD_APPSEC_ENABLED", "true")
 	t.Setenv("DD_APPSEC_RASP_ENABLED", "true")
 	t.Setenv("DD_APPSEC_WAF_TIMEOUT", "1h")
