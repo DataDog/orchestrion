3b2de2fa68a06c00ffec28de782d6b93154fe014
Rodrigo Arguello <rodrigo.arguello@datadoghq.com>
2024-08-30 11:36:13 +0200

    address review comments

--- _integration-tests/tests/aws.v2/base.go
+++ _integration-tests/tests/aws.v2/base.go
@@ -10,66 +10,27 @@ package awsv2
 import (
 	"context"
 	"fmt"
-	"os"
-	"runtime"
 	"testing"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/docker/go-connections/nat"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 
 	"orchestrion/integration/utils"
 	"orchestrion/integration/validator/trace"
 )
 
 type base struct {
-	server     testcontainers.Container
-	cfg        aws.Config
-	hostIP     string
-	mappedPort string
+	server testcontainers.Container
+	cfg    aws.Config
+	host   string
+	port   string
 }
 
 func (b *base) setup(t *testing.T) {
-	if _, ok := os.LookupEnv("CI"); ok && runtime.GOOS != "linux" {
-		t.Skip("skipping test as it requires docker to run in the CI")
-	}
-
-	port := "8000/tcp"
-	req := testcontainers.GenericContainerRequest{
-		ContainerRequest: testcontainers.ContainerRequest{
-			Image:        "amazon/dynamodb-local:latest",
-			ExposedPorts: []string{port},
-			WaitingFor:   wait.ForHTTP("").WithStatusCodeMatcher(func(int) bool { return true }),
-			Name:         "dynamodb-local",
-			WorkingDir:   "/home/dynamodblocal",
-			Cmd: []string{
-				"-jar", "DynamoDBLocal.jar",
-				"-inMemory",
-				"-disableTelemetry",
-			},
-			LogConsumerCfg: &testcontainers.LogConsumerConfig{
-				Consumers: []testcontainers.LogConsumer{utils.TestLogConsumer(t)},
-			},
-		},
-		Started: true,
-		Logger:  testcontainers.TestLogger(t),
-	}
-
-	ctx := context.Background()
-	var err error
-	b.server, err = testcontainers.GenericContainer(ctx, req)
-	require.NoError(t, err)
-
-	mappedPort, err := b.server.MappedPort(ctx, nat.Port(port))
-	require.NoError(t, err)
-	b.mappedPort = mappedPort.Port()
-
-	b.hostIP, err = b.server.Host(ctx)
-	require.NoError(t, err)
+	b.server, b.host, b.port = utils.StartDynamoDBTestContainer(t)
 }
 
 func (b *base) teardown(t *testing.T) {
@@ -79,9 +40,6 @@ func (b *base) teardown(t *testing.T) {
 }
 
 func (b *base) run(t *testing.T) {
-	if len(b.cfg.APIOptions) == 0 {
-		t.Log("the AWS config is not traced")
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 	ddb := dynamodb.NewFromConfig(b.cfg)
@@ -103,10 +61,8 @@ func (b *base) expectedSpans() trace.Spans {
 				"aws.region":    "test-region-1337",
 				"aws_service":   "DynamoDB",
 				"http.method":   "POST",
-				// TODO: investigate why this tag is not being set
-				// "http.status_code": "200",
-				"component": "aws/aws-sdk-go-v2/aws",
-				"span.kind": "client",
+				"component":     "aws/aws-sdk-go-v2/aws",
+				"span.kind":     "client",
 			},
 			Children: []*trace.Span{
 				{
@@ -119,7 +75,7 @@ func (b *base) expectedSpans() trace.Spans {
 					Meta: map[string]any{
 						"http.method":              "POST",
 						"http.status_code":         "200",
-						"http.url":                 fmt.Sprintf("http://localhost:%s/", b.mappedPort),
+						"http.url":                 fmt.Sprintf("http://localhost:%s/", b.port),
 						"network.destination.name": "localhost",
 						"component":                "net/http",
 						"span.kind":                "client",
