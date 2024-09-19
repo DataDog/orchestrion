2cf39c48389d7310ac0368aa2bfd8a243ae4906f
Romain Marcadier <romain@datadoghq.com>
2024-09-19 15:24:10 +0200

    chore: add benchmark to measure cost of use

--- go.mod
+++ go.mod
@@ -20,8 +20,9 @@ require (
 	github.com/stretchr/testify v1.9.0
 	github.com/urfave/cli/v2 v2.27.4
 	golang.org/x/mod v0.20.0
-	golang.org/x/sys v0.24.0
-	golang.org/x/term v0.23.0
+	golang.org/x/perf v0.0.0-20240910214617-f1a715d501dd
+	golang.org/x/sys v0.25.0
+	golang.org/x/term v0.24.0
 	golang.org/x/tools v0.24.0
 	gopkg.in/DataDog/dd-trace-go.v1 v1.68.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -56,6 +57,7 @@ require (
 	github.com/IBM/sarama v1.43.3 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/Shopify/sarama v1.38.1 // indirect
+	github.com/aclements/go-moremath v0.0.0-20210112150236-f10218a38794 // indirect
 	github.com/alecthomas/chroma/v2 v2.14.0 // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/armon/go-radix v1.0.1-0.20221118154546-54df44f2176c // indirect
@@ -281,13 +283,13 @@ require (
 	go.uber.org/automaxprocs v1.5.3 // indirect
 	gocloud.dev v0.39.0 // indirect
 	golang.org/x/arch v0.9.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/exp v0.0.0-20240823005443-9b4947da3948 // indirect
-	golang.org/x/image v0.19.0 // indirect
-	golang.org/x/net v0.28.0 // indirect
-	golang.org/x/oauth2 v0.22.0 // indirect
+	golang.org/x/image v0.20.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/oauth2 v0.23.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	golang.org/x/time v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240716161551-93cc26a95ae9 // indirect
 	google.golang.org/api v0.194.0 // indirect
---
+++ main_test.go
@@ -0,0 +1,214 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023-present Datadog, Inc.
+
+package main_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/mod/semver"
+)
+
+type testCase interface {
+	baseline()
+	instrumented()
+}
+
+var testCases = map[string]func(b *testing.B) testCase{
+	"DataDog:orchestrion": benchmarkOrchestrion,
+	"traefik:traefik":     benchmarkTraefik,
+}
+
+func Benchmark(b *testing.B) {
+	for name, create := range testCases {
+		tc := create(b)
+		b.Run(fmt.Sprintf("repo=%s/variant=baseline", name), func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				tc.baseline()
+			}
+		})
+
+		b.Run(fmt.Sprintf("repo=%s/variant=instrumented", name), func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				tc.instrumented()
+			}
+		})
+	}
+}
+
+type benchTraefik struct {
+	harness
+}
+
+func benchmarkTraefik(b *testing.B) testCase {
+	b.Helper()
+
+	tc := &benchTraefik{harness{B: b, build: "./..."}}
+
+	tag := tc.findLatestGithubReleaseTag("traefik", "traefik")
+	tc.gitCloneGithub("traefik", "traefik", tag)
+	tc.exec("go", "mod", "download")
+	tc.exec("go", "mod", "edit", "-replace", fmt.Sprintf("github.com/DataDog/orchestrion=%s", rootDir))
+	tc.exec(buildOrchestrion(b), "pin")
+
+	return tc
+}
+
+type benchOrchestrion struct {
+	harness
+}
+
+func benchmarkOrchestrion(b *testing.B) testCase {
+	return &benchOrchestrion{harness{B: b, dir: rootDir, build: "."}}
+}
+
+type harness struct {
+	*testing.B
+	dir   string // The directory in which the source code of the package to be built is located.
+	build string // The package to be built as part of the test.
+}
+
+func (h *harness) baseline() {
+	h.Helper()
+
+	cmd := exec.Command("go", "build", "-o", h.TempDir(), h.build)
+	cmd.Dir = h.dir
+	cmd.Env = append(os.Environ(), "GOCACHE="+h.TempDir())
+	output := bytes.NewBuffer(make([]byte, 0, 4_096))
+	cmd.Stdout = output
+	cmd.Stderr = output
+
+	h.StartTimer()
+	err := cmd.Run()
+	h.StopTimer()
+
+	assert.NoError(h, err, "build failed:\n%s", output)
+}
+
+func (h *harness) instrumented() {
+	h.Helper()
+
+	cmd := exec.Command(buildOrchestrion(h.B), "go", "build", "-o", h.TempDir(), h.build)
+	cmd.Dir = h.dir
+	cmd.Env = append(os.Environ(), "GOCACHE="+h.TempDir())
+	output := bytes.NewBuffer(make([]byte, 0, 4_096))
+	cmd.Stdout = output
+	cmd.Stderr = output
+
+	h.StartTimer()
+	err := cmd.Run()
+	h.StopTimer()
+
+	assert.NoError(h, err, "build failed:\n%s", output)
+}
+
+func (h *harness) exec(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	cmd.Dir = h.dir
+	cmd.Env = append(os.Environ(), "GOCACHE="+h.TempDir())
+	output := bytes.NewBuffer(make([]byte, 0, 4_096))
+	cmd.Stdout = output
+	cmd.Stderr = output
+
+	require.NoError(h, cmd.Run(), "command failed: %s\n%s", cmd, output)
+}
+
+func (h *harness) findLatestGithubReleaseTag(owner string, repo string) string {
+	h.Helper()
+
+	// NB -- Default page size is 30, and releases are sorted by creation date... We should be able to rely on the tag
+	// we are looking for being present in the first page, ergo we don't bother traversing all pages.
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.github.com/repos/%s/%s/releases", owner, repo), nil)
+	require.NoError(h, err)
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+
+	// If a GITHUB_TOKEN is available, authenticate the request to enjoy higher request limits...
+	if token := os.Getenv("GITHUB_TOKEN"); token != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(h, err)
+	defer resp.Body.Close()
+
+	require.Equal(h, http.StatusOK, resp.StatusCode, "error response body:\n%s", contentString{resp.Body})
+
+	var payload []struct {
+		Prerelease bool   `json:"prerelease"`
+		TagName    string `json:"tag_name"`
+	}
+	require.NoError(h, json.NewDecoder(resp.Body).Decode(&payload))
+	require.NotEmpty(h, payload)
+
+	var tagName string
+	for _, release := range payload {
+		if release.Prerelease {
+			// We're excluding pre-releases, just because.
+			continue
+		}
+		if tagName == "" || semver.Compare(tagName, release.TagName) < 0 {
+			tagName = release.TagName
+		}
+	}
+
+	require.NotEmpty(h, tagName)
+
+	return tagName
+}
+
+func (h *harness) gitCloneGithub(owner string, repo string, tag string) string {
+	h.Helper()
+
+	h.dir = h.TempDir()
+	h.exec("git", "clone", "--depth=1", fmt.Sprintf("--branch=%s", tag), fmt.Sprintf("https://github.com/%s/%s.git", owner, repo), h.dir)
+
+	return h.dir
+}
+
+var (
+	orchestrionBinOnce sync.Once
+	orchestrionBin     string
+)
+
+func buildOrchestrion(b *testing.B) string {
+	b.Helper()
+
+	orchestrionBinOnce.Do(func() {
+		orchestrionBin = filepath.Join(rootDir, "bin", "orchestrion.exe")
+
+		cmd := exec.Command("go", "build", fmt.Sprintf("-o=%s", orchestrionBin), rootDir)
+		require.NoError(b, cmd.Run())
+	})
+
+	return orchestrionBin
+}
+
+type contentString struct{ io.Reader }
+
+func (c contentString) String() string {
+	data, _ := io.ReadAll(c)
+	return string(data)
+}
+
+var rootDir string
+
+func init() {
+	_, thisFile, _, _ := runtime.Caller(0)
+	rootDir = filepath.Dir(thisFile)
+}
